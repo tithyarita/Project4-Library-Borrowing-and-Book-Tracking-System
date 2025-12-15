653e9dfb245f3cf4d2bf090006bcfb3e6ac15a17
Sreypy <sreypydoeung27@gmail.com>
2025-12-15 15:06:42 +0700

    Week1

---
+++ library_brorrow_and_book_tracking/src/test/java/com/library/library_brorrow_and_book_tracking/LibraryBrorrowAndBookTrackingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.library.library_brorrow_and_book_tracking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryBrorrowAndBookTrackingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
