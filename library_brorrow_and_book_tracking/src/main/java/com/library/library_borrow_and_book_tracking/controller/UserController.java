8fcd95abe41da03bcf4baa6f19a69445bc27d098
Leangei <leangeimeng1602@gmail.com>
2026-01-04 11:24:53 +0700

    before checkout

--- library_brorrow_and_book_tracking/src/main/java/com/library/library_borrow_and_book_tracking/controller/UserController.java
+++ library_brorrow_and_book_tracking/src/main/java/com/library/library_borrow_and_book_tracking/controller/UserController.java
@@ -39,18 +39,17 @@ public class UserController {
 
     // ===== SEARCH BOOKS =====
     @GetMapping("/user/search")
-    public String search(@RequestParam(name = "q", required = false) String q, Model model) {
-    List<Book> books = libraryService.searchBooks(q);
-    model.addAttribute("books", books);
-    return "user/search";
+    public String search(@RequestParam(required = false) String q, Model model) {
+        List<Book> books = libraryService.searchBooks(q);
+        model.addAttribute("books", books);
+        return "user/search";
     }
-
     // ===== BORROW RECORDS =====
-    @GetMapping("/user/borrowRecords")
-    public String borrowRecords(Model model, Principal principal) {
-        model.addAttribute("borrowRecords", libraryService.getRecentBorrows(principal.getName()));
-        return "user/borrowRecords";
-    }
+    // @GetMapping("/user/borrowRecords")
+    // public String borrowRecords(Model model, Principal principal) {
+    //     model.addAttribute("borrowRecords", libraryService.getRecentBorrows(principal.getName()));
+    //     return "user/borrowRecords";
+    // }
 
     // ===== BOOK (RESERVATION) =====
   @GetMapping("/user/booking/{bookId}")
